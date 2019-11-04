a1d75f1cdb91ac06f7780134f05384e3c4a3a4af
Devan Terry <devanterry@Devans-MacBook-Pro.local>
2019-11-04 13:40:21 -0800

    finished JS to add list items and subsequently delete them

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
+
+
 $('#add-skill').on('click', function(evt){
     let mylist = $('#input').val();
-    $('#list').append('<li>'+mylist+'</li>');
+    $('#list').append('<li><button>X</button>'+' '+mylist+'</li>');
+    $('#input').val('');
+    return false;
 });
 
+$('ul').on('click', 'button', function(){
+    $(this).closest('li').remove();
+});
